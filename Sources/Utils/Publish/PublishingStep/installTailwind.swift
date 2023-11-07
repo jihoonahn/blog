061f6e01ed4b5f4336c761becb90207174ebc06d
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-08 03:54:57 +0900

    Update installTailwind

--- Sources/Utils/Publish/PublishingStep/installTailwind.swift
+++ Sources/Utils/Publish/PublishingStep/installTailwind.swift
@@ -3,9 +3,9 @@ extension PublishingStep {
         .installPlugin(
             .init(name: "Tailwind", installer: { context in
                 let rootDirectory = try! AbsolutePath(validating: try context.folder(at: "/").path)
+                try await tailwind.initialize()
                 try await tailwind.run(input: rootDirectory.appending(components: ["Style", "input.css"]),
                                        output: rootDirectory.appending(components: ["Output", "output.css"]))
-                try await tailwind.initialize()
             })
         )
     }
