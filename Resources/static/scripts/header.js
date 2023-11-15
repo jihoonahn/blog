ab71fe4b9d8096308a07b36743b7cbf1389c8fb6
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-15 19:43:57 +0900

    Update resource

--- Resources/static/scripts/header.js
+++ Resources/static/scripts/header.js
@@ -1,10 +1,23 @@
-const menuButton = document.querySelectorAll('.getElementsByClassName');
-
-function createBlogNavScreen() {
-    document.body.style.overflow = 'hidden';
+function attachEvent(selector, event, fn) {
+    const matches = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
+    if (matches && matches.length) {
+      matches.forEach((elem) => {
+        elem.addEventListener(event, (e) => fn(e, elem), false);
+      });
+    }
 }
 
-menuButton.forEach((element) => {
-    element.addEventListener('click', createBlogNavScreen);
-})
+window.onload = function() {
+    attachEvent('#mobileNavButton', 'click', function(_, elem) {
+        document.body.classList.toggle('overflow-hidden');
+        document.getElementById("header")?.classList.toggle("blogMobileNav");
+        document.querySelector("#header nav")?.classList.toggle("hidden");
+    });
+};
 
+window.onpageshow = function() {
+    const navButton = document.getElementById('mobileNavButton');
+    document.body.classList.remove("overflow-hidden");
+    document.getElementById("header")?.classList.remove("h-screen");
+    document.querySelector("#header nav")?.classList.add("hidden");
+};
