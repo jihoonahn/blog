ff203317c301ea429a3749d3711b98fe44db36ce
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-17 05:02:07 +0900

    Add 404Error Step

--- Sources/Utils/Publish/PublishingStep/404Error.swift
+++ Sources/Utils/Publish/PublishingStep/404Error.swift
@@ -1,8 +1,28 @@
-//
-//  File.swift
-//  
-//
-//  Created by Jihoonahn on 11/17/23.
-//
+import Publish
 
-import Foundation
+extension PublishingStep {
+    static func move404File() -> Self {
+        let stepName = "Move 404 file for Blog Pages"
+
+        return step(named: stepName) { context in
+            guard let orig404Page = context.pages["404"] else {
+                throw PublishingError(stepName: stepName,
+                                      infoMessage: "Unable to find 404 page")
+            }
+
+            let orig404File = try context.outputFile(at: "\(orig404Page.path)/index.html")
+            try orig404File.rename(to: "404")
+
+            guard
+                let orig404Folder = orig404File.parent,
+                let outputFolder = orig404Folder.parent,
+                let rootFolder = outputFolder.parent
+            else {
+                throw PublishingError(stepName: stepName,
+                                      infoMessage: "Unable find root, output and 404 folders")
+            }
+            try context.copyFileToOutput(from: "\(orig404File.path(relativeTo: rootFolder))")
+            try orig404Folder.delete()
+        }
+    }
+}
