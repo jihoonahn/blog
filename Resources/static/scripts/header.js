c34079ab6f0142e4d42e127a4e04ffc0c4580a33
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-17 03:14:44 +0900

    Update header js

--- Resources/static/scripts/header.js
+++ Resources/static/scripts/header.js
@@ -8,16 +8,27 @@ function attachEvent(selector, event, fn) {
 }
 
 window.onload = function() {
-    attachEvent('#mobileNavButton', 'click', function(_, elem) {
-        document.body.classList.toggle('overflow-hidden');
-        document.getElementById("header")?.classList.toggle("blogMobileNav");
-        document.querySelector("#header nav")?.classList.toggle("hidden");
-    });
+  let lastKnownInnerWidth = window.innerWidth
+  let ticking = true;
+
+  attachEvent('#mobileNavButton', 'click', function(_, elem) {
+    document.body.classList.toggle('overflow-hidden');
+    document.querySelector('#blogNavScreen')?.classList.toggle('hidden');
+  });
+
+  function applyHeaderStylesOnResize() {
+    if (lastKnownInnerWidth<768) {
+      document.body.classList.remove('overflow-hidden');
+      document.querySelector('#blogNavScreen')?.classList.add('hidden');
+    }
+  }
+
+  applyHeaderStylesOnResize();
+  window.addEventListener('resize', applyHeaderStylesOnResize);
 };
 
 window.onpageshow = function() {
     const navButton = document.getElementById('mobileNavButton');
-    document.body.classList.remove("overflow-hidden");
-    document.getElementById("header")?.classList.remove("h-screen");
-    document.querySelector("#header nav")?.classList.add("hidden");
+    document.body.classList.remove('overflow-hidden');
+    document.querySelector('#header nav')?.classList.add('hidden');
 };
