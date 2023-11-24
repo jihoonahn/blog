b643a67aa2e7ba5abab6df200644791b90e38673
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-25 04:49:30 +0900

    Update TagDetail -> Tag Details

--- Sources/Pages/tags/TagDetails.swift
+++ Sources/Pages/tags/TagDetails.swift
@@ -1,22 +1,20 @@
-struct TagDetailPage: Component {
+struct TagDetailsPage: Component {
     let items: [Item<Blog>]
     let context: PublishingContext<Blog>
-    let selectedTag: Tag?
+    let selectedTag: Tag
     let pageNumber: Int
 
     var body: Component {
         Section {
             Div {
                 ArchiveLayout(
-                    title: selectedTag?.string ?? "Tag Archive",
+                    title: selectedTag.string,
                     items: items,
                     context: context
                 )
                 if items.count > Constants.numberOfItemsPerTagsPage || pageNumber > 1 {
-                    if let selectedTag {
-                        Pagination(activePage: pageNumber, numberOfPages: context.paginatedItems(for: selectedTag).count) { num in
-                            context.site.paginatedPath(for: selectedTag, pageIndex: num - 1).absoluteString
-                        }
+                    Pagination(activePage: pageNumber, numberOfPages: context.paginatedItems(for: selectedTag).count) { num in
+                        context.site.paginatedPath(for: selectedTag, pageIndex: num - 1).absoluteString
                     }
                 }
             }
