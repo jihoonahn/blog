2ebe8b5795e2839bb76e4922dc9d4c22cdd696b0
jihoonahn <jihoonahn.dev@gmail.com>
2023-11-28 02:21:15 +0900

    Update docsearch

--- Resources/static/scripts/docsearch.js
+++ Resources/static/scripts/docsearch.js
@@ -1,7 +1,23 @@
+const currentPageURL = window.location.href;
+let lvl0Value;
+
+if (currentPageURL.includes('/blog')) {
+  lvl0Value = 'Blog';
+} else if (currentPageURL.includes('/about')) {
+  lvl0Value = 'About';
+} else if (currentPageURL.includes('/tags')) {
+  lvl0Value = 'Tag';
+} else {
+  lvl0Value = 'jihoon.me';
+}
+
 docsearch({
     container: '#docsearch',
     appId: '3FO4WHQGGO',
     indexName: 'blog',
     apiKey: 'f45f471cd92d448f891637e486f50362',
+    algoliaOptions: {
+      facetFilters: [`lvl0:${lvl0Value}`],
+    },
     debug: true
 });
