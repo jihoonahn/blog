4a4812b80baf18fcd7be21a404fb375bbb5c70c0
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-04 17:12:21 +0900

    Update Tag Text

--- Sources/Layouts/ArchiveLayout.swift
+++ Sources/Layouts/ArchiveLayout.swift
@@ -12,7 +12,7 @@ struct ArchiveLayout: Component {
                     }
                     .class("my-auto border border-gray-200 rounded-lg md:rounded-2xl overflow-hidden self-start w-[30%] md:w-[350px]")
                     Div {
-                        Paragraph(item.tags.map{ $0.string }.joined(separator: ","))
+                        Paragraph(item.tags.map{ $0.string }.joined(separator: ", "))
                             .class("text-blog-c-tag-text text-xs md:text-sm text-black m-0 break-all")
                         H3(item.title)
                             .class("text-black text-base md:text-heading-3 mb-2 mt-2 break-all")
