3a959e5f949630b8c4264f9b299402b8bc0dda35
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-10 14:41:18 +0900

    Update Package

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Publish", from: "0.9.0"),
-        .package(url: "https://github.com/JohnSundell/SplashPublishPlugin", from: "0.2.0")
+        .package(url: "https://github.com/Johnsundell/Splash", from: "0.16.0")
     ],
     targets: [
         .executableTarget(
             name: "Blog",
             dependencies: [
                 .product(name: "Publish", package: "publish"),
-                .product(name: "SplashPublishPlugin", package: "SplashPublishPlugin")
+                .product(name: "Splash", package: "Splash")
             ],
             path: "Sources",
             exclude: ["Styles/global.css"]
---
+++ Sources/Utils/Publish/PublishingStep/CodeBlock.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jihoonahn on 12/10/23.
+//
+
+import Foundation
