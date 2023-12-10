d08590c9a6e7f029dd02bf629f68a3d5da88db2f
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-10 14:42:06 +0900

    Add CodeBlock Modifier

--- Sources/Utils/Publish/PublishingStep/CodeBlock.swift
+++ Sources/Utils/Publish/PublishingStep/CodeBlock.swift
@@ -1,8 +1,31 @@
-//
-//  File.swift
-//  
-//
-//  Created by Jihoonahn on 12/10/23.
-//
+extension PublishingStep where Site == Blog {
+    static func codeBlock(_ classPrefix: String = "") -> Self {
+        step(named: "CodeBlock") { step in
+            step.markdownParser.addModifier(
+                .codeBlock(HTMLOutputFormat(classPrefix: classPrefix))
+            )
+        }
+    }
+}
 
-import Foundation
+extension Modifier {
+    static func codeBlock(_ format: HTMLOutputFormat = HTMLOutputFormat()) -> Self {
+        let highlighter = SyntaxHighlighter(format: format)
+
+        return Modifier(target: .codeBlocks) { html, markdown in
+            var markdown = markdown.dropFirst("```".count)
+
+            guard !markdown.hasPrefix("no-highlight") else {
+                return html
+            }
+
+            markdown = markdown
+                .drop(while: { !$0.isNewline })
+                .dropFirst()
+                .dropLast("\n```".count)
+
+            let highlighted = highlighter.highlight(String(markdown))
+            return "<pre><code>" + highlighted + "\n</code></pre>"
+        }
+    }
+}
