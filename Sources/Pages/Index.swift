8f39b698222da56a8dae7e68a347000091d75a7e
Jihoon Ahn <jihoonahn.dev@gmail.com>
2024-04-18 22:30:34 +0900

    Update Index.swift

--- Sources/Pages/Index.swift
+++ Sources/Pages/Index.swift
@@ -3,13 +3,12 @@ struct IndexPage: Component {
     let context: PublishingContext<Blog>
 
     var body: Component {
-        PageLayout(title: "Posts") {
-            ComponentGroup {
-                PostsLayout(items: context.paginatedItems[pageNumber-1], context: context)
-                Pagination(activePage: pageNumber, numberOfPages: context.paginatedItems.count) { num in
-                    context.index.paginatedPath(pageIndex: num - 1).absoluteString
-                }
+        Section  {
+            PostsLayout(items: context.paginatedItems[pageNumber-1], context: context)
+            Pagination(activePage: pageNumber, numberOfPages: context.paginatedItems.count) { num in
+                context.index.paginatedPath(pageIndex: num - 1).absoluteString
             }
         }
+        .class("px-4 sm:px-8 max-w-3xl mx-auto")
     }
 }
