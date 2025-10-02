ef2bfbd35411d38fb19b98454be6b5d1af682690
jihoonahn <jihoonahn.dev@gmail.com>
2025-10-02 23:24:35 +0900

    Delete .astro

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
-		
--- .astro/fonts.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'astro:assets' {
-	/** @internal */
-	export type CssVariable = (["--font-inter"])[number];
-}
