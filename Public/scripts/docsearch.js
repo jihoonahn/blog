de6f0170d0f3e452c4862a8decc30dba6df5dd28
Jihoon Ahn <jihoonahn.dev@gmail.com>
2025-10-03 21:33:38 +0900

    Update docsearch.js

--- Public/scripts/docsearch.js
+++ Public/scripts/docsearch.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   if (typeof docsearch !== "undefined") {
     docsearch({
       container: "#docsearch",
-      appId: "3FO4WHQGGO",
-      indexName: "blog",
-      apiKey: "f45f471cd92d448f891637e486f50362",
+      appId: "46IWXDIK1T",
+      indexName: "website",
+      apiKey: "cc465f762d87fbe1c53554e511ae67e6",
       debug: true,
     });
   }
