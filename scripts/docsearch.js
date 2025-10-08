23fcbedb0475081829c2e9d0b4c088b209041b5d
jihoonahn <jihoonahn@users.noreply.github.com>
2025-10-08 11:30:37 +0000

    deploy: 46788411f133b4b916702c076bf3c6f240939c88

---
+++ scripts/docsearch.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (typeof docsearch !== "undefined") {
+    docsearch({
+      container: "#docsearch",
+      appId: "DOAQK3DEFT",
+      indexName: "post-crawler",
+      apiKey: "f2f6b3d392f55f48aa6f9d3939f2cd98",
+      debug: true,
+    });
+  }
+});
